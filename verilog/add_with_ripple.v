0ac8bcf8ef5b7ea6e586e8857805caf16e2814c7
acz <zeitnot@yahoo.com>
2022-07-17 15:57:13 -0500

    Corrected code for compatibility with 8-bit IDE (https://8bitworkshop.com)

--- verilog/add_with_ripple.v
+++ verilog/add_with_ripple.v
@@ -1,56 +1,37 @@
 // Our eighth Verilog circuit where we connect two adders into
 // a ripple adder, allowing us to add two-digit binary numbers.
 
-module add_with_carry(a, b, y, c_in, c_out);
-  input a;
-  input b;
-  input c_in;
-  output y;
-  output c_out;
-  
-  wire w1;
-  wire w2;
-  wire w3;
-  
-  assign w1 = a ^ b;
-  assign w2 = w1 & c_in;
-  assign w3 = a & b;
-  
-  assign y = w1 ^ c_in;
-  assign c_out = w2 | w3;
-endmodule
+`include "full_add_with_carry.v"
 
-
-// Next, we declare two full adders in a cascading sequence.
-// The first adder in the sequence has no carry input, so we
-// set its carry input to zero.
-
-module ripple_adder(a, b, y);
-  input [1:0] a;
-  input [1:0] b;
-  output [2:0] y;
+module ripple_adder
+  (
+    input [1:0] a,
+    input [1:0] b,
+    output [2:0] y
+  );
   
-  wire [2:0] c;
+  wire c1, c2, c3;
   wire [1:0] sum;
   
+  assign c1 = 0;
   
   add_with_carry adder_1
   (
     .a(a[0]),
     .b(b[0]),
-    .c_in(c[0]),
+    .c_in(c1),
     .y(sum[0]),
-    .c_out(c[1])
+    .c_out(c2)
   );
   
   add_with_carry adder_2
   (
     .a(a[1]),
     .b(b[1]),
-    .c_in(c[1]),
+    .c_in(c2),
     .y(sum[1]),
-    .c_out(c[2])
+    .c_out(c3)
   );
   
-  assign y = {c[2], sum};
+  assign y = {c3, sum};
 endmodule
