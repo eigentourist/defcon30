9eedae94161eed7b19230d9ed7ef77b5eedeeee2
eigentourist <eigentourist@gmail.com>
2022-08-11 16:13:16 -0500

    renamed obj and exe files for factorial asm

--- asm/build-factorial.bat
+++ asm/build-factorial.bat
@@ -1,2 +1,2 @@
-nasm -f win64 -o factorial-asm.obj factorial.asm
-link factorial-asm.obj /subsystem:console /out:factorial-asm.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
+nasm -f win64 -o factorial.obj factorial.asm
+link factorial.obj /subsystem:console /out:factorial.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
